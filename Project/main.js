f048e3b468d6d71a1fc9f677878ed43a4f187853
andyxhliu <andyxhliu@outlook.com>
2016-07-08 14:39:03 +0100

    Upgrade event listener to drag identifier

--- Project/main.js
+++ Project/main.js
@@ -3,6 +3,7 @@ document.body.onload = addContainer;
 var container = document.createElement('div');
 var blocksArray=[];
 var block;
+var flag=0;
 
 
 function addContainer() {
@@ -15,21 +16,15 @@ function addBlocksToListen() {
   var blockNumber = 36;
   for (var i=0; i<36; i++) {
     block = document.createElement('div');
-    // var callback = function () {
-    //   block.addEventListener("mouseover", function() {
-    //   console.log("Hello");
-    // });
     block.setAttribute("class", "block");
     container.appendChild(block);
     blocksArray.push(block);
+    // block.addEventListener("mouseup", function(){
+    //   flag=1;
+    // }, false);
   }
-  // block.setAttribute("class", "block");
-  // container.appendChild(block);
-  // blocksArray.push(block);
   blocksStarter();
 }
-  // blocksStarter();
-
 
 function blocksStarter() {
   blocksArray[0].setAttribute("id", "blockRed");
@@ -39,12 +34,22 @@ function blocksStarter() {
 function addEventListener() {
   blockRed.addEventListener("mousedown", function() {
     for (var i=0; i<36; i++) {
-      var blockId = document.getElementsByClassName("block")[i];  
+      var blockId = document.getElementsByClassName("block")[i]; 
       blockId.addEventListener("mouseover", function() {
+      determineDragOrMove(); 
+        if (flag===0) {
           console.log("Hello");
+        } 
       });
     };
   });
 }
 
-// .addEventListener("click", myFunction);
+function determineDragOrMove() {
+  for (var i=0; i<36; i++) {
+  var blocks = document.getElementsByClassName("block")[i];
+  blocks.addEventListener("mouseup", function(){
+    flag=1;
+    }, false);
+  }
+}
