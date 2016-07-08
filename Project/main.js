73b2fa0bc9721e969ecba139411bd94ede421d9b
andyxhliu <andyxhliu@outlook.com>
2016-07-08 13:52:12 +0100

    Add event listener for mouse moving over, and only activate after clicked on correct block

--- Project/main.js
+++ Project/main.js
@@ -2,24 +2,49 @@ document.body.onload = addContainer;
 
 var container = document.createElement('div');
 var blocksArray=[];
+var block;
+
 
 function addContainer() {
   container.setAttribute("class", "container");
   document.body.appendChild(container);
-  addBlocks();
+  addBlocksToListen();
 }
 
-function addBlocks() {
+function addBlocksToListen() {
   var blockNumber = 36;
   for (var i=0; i<36; i++) {
-    var block = document.createElement('div');
+    block = document.createElement('div');
+    // var callback = function () {
+    //   block.addEventListener("mouseover", function() {
+    //   console.log("Hello");
+    // });
     block.setAttribute("class", "block");
     container.appendChild(block);
     blocksArray.push(block);
   }
+  // block.setAttribute("class", "block");
+  // container.appendChild(block);
+  // blocksArray.push(block);
   blocksStarter();
 }
+  // blocksStarter();
+
 
 function blocksStarter() {
   blocksArray[0].setAttribute("id", "blockRed");
+  addEventListener();
 }
+
+function addEventListener() {
+  blockRed.addEventListener("mousedown", function() {
+    for (var i=0; i<36; i++) {
+      var blockId = document.getElementsByClassName("block")[i];  
+      blockId.addEventListener("mouseover", function() {
+          console.log("Hello");
+      });
+    };
+  });
+}
+
+// .addEventListener("click", myFunction);
