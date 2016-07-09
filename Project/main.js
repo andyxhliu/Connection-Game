67815e67769f414722e01af34c867c03ed5e0fba
andyxhliu <andyxhliu@outlook.com>
2016-07-09 15:10:53 +0100

    add all colors on the board and create a timer

--- Project/main.js
+++ Project/main.js
@@ -1,15 +1,37 @@
 document.body.onload = addContainer;
 
+setTimeout(function() {
+  console.log("Timeout function called");
+}, 2000);
+
+var time=15;
+var timerId=setInterval(function() {
+  time--;
+  addTimerDisplay();
+  if (time===0) {
+    clearInterval(timerId);
+  }
+}, 1000);
+
 var container = document.createElement('div');
 var resetButton = document.createElement('div');
 var helpButton = document.createElement('div');
 var helpScreen = document.createElement('div');
 var blocks=document.createElement('div');
 var backButton = document.createElement('div');
+var timerDisplay = document.createElement('div');
 var blocksArray=[];
 var block;
 var j;
 var color;
+
+
+function addTimerDisplay() {
+  timerDisplay.setAttribute("class", "timerDisplay");
+  document.body.insertBefore(timerDisplay,container);
+  timerDisplay.innerHTML=time;
+}
+
 
 function addContainer() {
   container.setAttribute("class", "container");
@@ -34,49 +56,85 @@ function addBlocksToListen() {
 
 function blocksStarter() {
   blocksArray[0].setAttribute("id", "initRed");
+  blocksArray[0].setAttribute("class", "starter");
   blocksArray[24].setAttribute("id", "endRed");
+  blocksArray[24].setAttribute("class", "starter");
+
   blocksArray[8].setAttribute("id", "initBlue");
+  blocksArray[8].setAttribute("class", "starter");
   blocksArray[15].setAttribute("id", "endBlue");
+  blocksArray[15].setAttribute("class", "starter");
+
   blocksArray[14].setAttribute("id", "initGreen");
+  blocksArray[14].setAttribute("class", "starter");
   blocksArray[35].setAttribute("id", "endGreen");
-  addEventListener(initRed, "endRed", "initBlue", "endBlue", "initGreen", "endGreen");
-  addEventListener(initBlue, "endBlue", "initRed", "initRed", "initGreen", "endGreen");
-  addEventListener(initGreen, "endGreen", "initRed", "initRed", "initBlue", "endBlue");
+  blocksArray[35].setAttribute("class", "starter");
+
+  blocksArray[30].setAttribute("id", "initPurple");
+  blocksArray[30].setAttribute("class", "starter");
+  blocksArray[28].setAttribute("id", "endPurple");
+  blocksArray[28].setAttribute("class", "starter");
+
+  blocksArray[10].setAttribute("id", "initYellow");
+  blocksArray[10].setAttribute("class", "starter");
+  blocksArray[26].setAttribute("id", "endYellow");
+  blocksArray[26].setAttribute("class", "starter");
+
+  blocksArray[20].setAttribute("id", "initAqua");
+  blocksArray[20].setAttribute("class", "starter");
+  blocksArray[25].setAttribute("id", "endAqua");
+  blocksArray[25].setAttribute("class", "starter");
+  addEventListenerMouseDown(initRed);
+  addEventListenerMouseDown(initBlue);
+  addEventListenerMouseDown(initGreen);
+  addEventListenerMouseDown(initPurple);
+  addEventListenerMouseDown(initYellow);
+  addEventListenerMouseDown(initAqua);
 }
 
-function addEventListener(init, end, OtherColorAI, OtherColorAE, OtherColorBI, OtherColorBE) {
+// function blocksStarter(positionInit, init, positionEnd, end) {
+//   this.positionInit=positionInit;
+//   this.init=init;
+//   this.positionEnd=positionEnd;
+//   this.end=end;
+// }
+
+// blocksStarter.prototype.setAttri=function() {
+//   blocksArray[positionInit].setAttribute("id", init);
+//   blocksArray[positionInit].setAttribute("class", "starter");
+//   blocksArray[positionEnd].setAttribute("id", end);
+//   blocksArray[positionEnd].setAttribute("class", "starter");
+//   addEventListenerMouseDown(initRed);
+//   addEventListenerMouseDown(initBlue);
+//   addEventListenerMouseDown(initGreen);
+// }
+
+// var red= new blocksStarter(0,"initRed",24, "endRed");
+// var blue= new blocksStarter(8,"initBlue", 15, "endBlue");
+// var green= new blocksStarter(14,"initgreen", 35, "endGreen");
+
+function addEventListenerMouseDown(init) {
   init.addEventListener("mousedown", function() {
-
     color = this.id.replace("init", "").toLowerCase();
-
     this.className = "block"+" "+ color;
-    blocksArray.forEach(function(block, i) {
-      block.addEventListener("mouseover", function() {
-        if(event.buttons === 1 &&
-          this.id!==OtherColorAI &&
-          this.id!==OtherColorAE &&
-          this.id!==OtherColorBI &&
-          this.id!==OtherColorBE &&
-          this.className==="block" &&
-          (blocksArray[i-1] && blocksArray[i-1].className === "block"+" "+ color ||
-            blocksArray[i-6] && blocksArray[i-6].className === "block"+" "+ color ||
-            blocksArray[i+1].className === "block"+" "+ color ||
-            blocksArray[i+6].className === "block"+" "+ color)
-          ) {
-          this.className = "block"+" "+ color;
-          if (this.id===end) {
-            console.log("You found it");
-            /*How to disable the event listener? So user won't go pass it?*/
-          }
-          checkWin();
-        // } else if (event.buttons===0) {
-        //   blocksArray.forEach(function(block,i) {
-        //     var toBeCleared = document.getElementsByClassName("block"+" "+ Color);
-        //     toBeCleared.className="block";
-        //   })  
-          /*How to istinguish whether or not mouse ip Yet?*/
-        }
-      });
+  });
+  addEventListenerMouseOver();
+}
+
+function addEventListenerMouseOver() {
+  blocksArray.forEach(function(block, i) {
+    block.addEventListener("mouseover", function() {
+      if(event.buttons === 1 &&
+        this.className!=="starter" &&
+        this.className==="block" &&
+        (blocksArray[i-1] && blocksArray[i-1].className === "block"+" "+ color ||
+          blocksArray[i-6] && blocksArray[i-6].className === "block"+" "+ color ||
+          blocksArray[i+1].className === "block"+" "+ color ||
+          blocksArray[i+6].className === "block"+" "+ color)
+        ) {
+        this.className = "block"+" "+ color;
+        checkWin();
+      }
     });
   });
 }
@@ -87,7 +145,7 @@ function checkWin() {
     if(blocksArray[j].className!=="block"){
       counter++;
     }
-    if (counter===(blocksArray.length-4)) {
+    if (counter===(blocksArray.length)) {
       console.log("You win!");
     }
   }
@@ -135,6 +193,9 @@ function allowBackToListen() {
   console.log("clear it");
   }) 
 }
+
+
+
 
 
 
