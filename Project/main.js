05219db4763bc147eb99b69fe9733c40987f726b
andyxhliu <andyxhliu@outlook.com>
2016-07-09 10:37:00 +0100

    complete for win condition

--- Project/main.js
+++ Project/main.js
@@ -3,6 +3,7 @@ document.body.onload = addContainer;
 var container = document.createElement('div');
 var blocksArray=[];
 var block;
+var j;
 
 function addContainer() {
   container.setAttribute("class", "container");
@@ -22,19 +23,61 @@ function addBlocksToListen() {
 }
 
 function blocksStarter() {
-  blocksArray[0].setAttribute("id", "blockRed");
+  blocksArray[0].setAttribute("id", "initRed");
   addEventListener();
 }
 
 function addEventListener() {
-  blockRed.addEventListener("mousedown", function() {
-    this.className = "block selected";
+  initRed.addEventListener("mousedown", function() {
+    this.className = "block red";
     blocksArray.forEach(function(block, i) {
       block.addEventListener("mouseover", function() {
-        if(event.buttons === 1 && (blocksArray[i-1].className === "block selected"||blocksArray[i-6].className === "block selected" ||blocksArray[i+1].className === "block selected" || blocksArray[i+6].className === "block selected")) {
-          this.className = "block selected";
-        }
+        if(event.buttons === 1 && (blocksArray[i-1].className === "block red"||blocksArray[i-6].className === "block red" ||blocksArray[i+1].className === "block red" || blocksArray[i+6].className === "block red")) {
+          this.className = "block red";
+          checkWin();
+        } 
       });
     });
   });
 }
+
+function checkWin() {
+  var counter=0;
+  for (var j=0; j<blocksArray.length; j++) {
+    if(blocksArray[j].className!=="block"){
+      counter++;
+    }
+    if (counter===(blocksArray.length)) {
+      console.log("You win!");
+    }
+  }
+}
+
+/*Above is for win condition completed*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
