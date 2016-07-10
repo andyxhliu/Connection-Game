2cfa59f8106601ec6a8b865768f48dc1151539d3
andyxhliu <andyxhliu@outlook.com>
2016-07-10 21:47:20 +0100

    allow change levels

--- Project/main.js
+++ Project/main.js
@@ -1,4 +1,8 @@
-document.body.onload = addContainer;
+document.body.onload = welcomeScreen;
+
+function welcomeScreen() {
+  addContainer();
+}
 
 setTimeout(function() {
   console.log("Timeout function called");
@@ -6,26 +10,26 @@ setTimeout(function() {
 
 var time=20;
 
-function startTiming() {
-  var timerId=setInterval(function() {
-    time--;
-    console.log(time);
-    addTimerDisplay();
-    if (time===0 || result==="win") {
-      result=null;
-      clearInterval(timerId);
-      if (time===0) {
-        // clearInterval(timerId);
-        // result=null;
-        display.setAttribute("class","display"+" lose");
-        buttonNext.setAttribute("class", "buttonNext");
-        buttonReplay.setAttribute("class", "buttonReplay"+" win");
-        addButtonReplayToListen();
-        reset(allColors);
-      }
-    }
-  }, 1000);
-}
+// function startTiming() {
+//   var timerId=setInterval(function() {
+//     time--;
+//     console.log(time);
+//     addTimerDisplay();
+//     if (time===0 || result==="win") {
+//       result=null;
+//       clearInterval(timerId);
+//       if (time===0) {
+//         // clearInterval(timerId);
+//         // result=null;
+//         display.setAttribute("class","display"+" lose");
+//         buttonNext.setAttribute("class", "buttonNext");
+//         buttonReplay.setAttribute("class", "buttonReplay"+" win");
+//         addButtonReplayToListen();
+//         reset(allColors);
+//       }
+//     }
+//   }, 1000);
+// }
 
 var container = document.createElement('div');
 var resetButton = document.createElement('div');
@@ -44,17 +48,162 @@ var j;
 var color;
 var result=null;
 var level=0;
-var initialPositions=[[0,24,8,15,14,35,30,28,10,26,20,25],
-                      [0,19,5,9,4,29,27,35,6,33,25,22],
-                      [0,28,10,14,1,17,31,26,30,12,32,15],
-                      [1,6,12,7,2,31,25,35,10,14,15,22]];
-var initialPlace=[];
-
-function chooseInitialPosition(level) {
-  for (var i=0; i<initialPositions[level].length; i++) {
-    initialPlace.push(initialPositions[level][i]);
+var initialPositions=[
+  {
+    initRed: 0,
+    endRed: 24,
+    initBlue: 8,
+    endBlue: 15,
+    initGreen: 14,
+    endGreen: 35,
+    initPurple: 30,
+    endPurple: 28,
+    initYellow: 10,
+    endYellow: 26,
+    initAqua:20,
+    endAqua:25
+  },
+  { 
+    initRed: 0,
+    endRed: 19,
+    initBlue:5,
+    endBlue:9,
+    initGreen:4,
+    endGreen:29,
+    initPurple:27,
+    endPurple:35,
+    initYellow:6,
+    endYellow:33,
+    initAqua:25,
+    endAqua:22
+  },
+  {
+    initRed: 0,
+    endRed: 19,
+    initBlue:5,
+    endBlue:9,
+    initGreen:4,
+    endGreen:29,
+    initPurple:27,
+    endPurple:35,
+    initYellow:6,
+    endYellow:33,
+    initAqua:25,
+    endAqua:22
+  },
+  {
+    initRed:0,
+    endRed:28,
+    initBlue:10,
+    endBlue:14,
+    initGreen:1,
+    endGreen:17,
+    initPurple:31,
+    endPurple:26,
+    initYellow:30,
+    endYellow:12,
+    initAqua:32,
+    endAqua:15
+  },
+  {
+    initRed:1,
+    endRed:6,
+    initBlue:12,
+    endBlue:7,
+    initGreen:2,
+    endGreen:31,
+    initPurple:25,
+    endPurple:35,
+    initYellow:10,
+    endYellow:14,
+    initAqua:15,
+    endAqua:22
+  },
+  {
+    initRed:34,
+    endRed:12,
+    initBlue:5,
+    endBlue:35,
+    initGreen:4,
+    endGreen:22,
+    initPurple:18,
+    endPurple:33,
+    initYellow:26,
+    endYellow:7,
+    initAqua:20,
+    endAqua:8
+  },
+  {
+    initRed:0,
+    endRed:8,
+    initBlue:24,
+    endBlue:6,
+    initGreen:5,
+    endGreen:30,
+    initPurple:16,
+    endPurple:35,
+    initYellow:10,
+    endYellow:29,
+    initAqua:12,
+    endAqua:19
+  },
+  {
+    initRed:10,
+    endRed:0,
+    initBlue:4,
+    endBlue:27,
+    initGreen:1,
+    endGreen:8,
+    initPurple:30,
+    endPurple:19,
+    initYellow:35,
+    endYellow:5,
+    initAqua:12,
+    endAqua:24
+  },
+  {
+    initRed:20,
+    endRed:0,
+    initBlue:6,
+    endBlue:14,
+    initGreen:33,
+    endGreen:5,
+    initPurple:18,
+    endPurple:27,
+    initYellow:11,
+    endYellow:35,
+    initAqua:25,
+    endAqua:12
+  },
+  {
+    initRed:12,
+    endRed:0,
+    initBlue:23,
+    endBlue:20,
+    initGreen:31,
+    endGreen:17,
+    initPurple:28,
+    endPurple:35,
+    initYellow:27,
+    endYellow:15,
+    initAqua:25,
+    endAqua:34
+  },
+  {
+    initRed:0,
+    endRed:3,
+    initBlue:23,
+    endBlue:5,
+    initGreen:4,
+    endGreen:6,
+    initPurple:25,
+    endPurple:34,
+    initYellow:31,
+    endYellow:22,
+    initAqua:35,
+    endAqua:19
   }
-}
+];
 
 function addTimerDisplay() {
   timerDisplay.setAttribute("class", "timerDisplay");
@@ -68,11 +217,14 @@ function addContainer() {
   document.body.appendChild(container);
   blocks.setAttribute("class", "blocks");
   document.getElementsByClassName("container")[0].appendChild(blocks);
-  chooseInitialPosition(level);
   addResetButton();
   addBlocksToListen();
-  addHelpButton();
+  addButtonNextToListen();
+  addButtonReplayToListen();
+  addResetButton();
   addEventListenerMouseUp();
+  addHelpButton();
+  blocksStarter();
   addDisplay();
   addButtonNext();
   addButtonReplay();
@@ -102,56 +254,57 @@ function addBlocksToListen() {
     document.getElementsByClassName("blocks")[0].appendChild(block);
     blocksArray.push(block);
   }
-  blocksStarter();
 }
 
 function blocksStarter() {
-  blocksArray[0].setAttribute("id", "initRed");
-  blocksArray[0].setAttribute("class", "starter");
-  blocksArray[24].setAttribute("id", "endRed");
-  blocksArray[24].setAttribute("class", "starter");
+  blocksArray[initialPositions[level].initRed].setAttribute("id", "initRed");
+  blocksArray[initialPositions[level].initRed].setAttribute("class", "starter");
+  blocksArray[initialPositions[level].endRed].setAttribute("id", "endRed");
+  blocksArray[initialPositions[level].endRed].setAttribute("class", "starter");
 
-  blocksArray[8].setAttribute("id", "initBlue");
-  blocksArray[8].setAttribute("class", "starter");
-  blocksArray[15].setAttribute("id", "endBlue");
-  blocksArray[15].setAttribute("class", "starter");
+  blocksArray[initialPositions[level].initBlue].setAttribute("id", "initBlue");
+  blocksArray[initialPositions[level].initBlue].setAttribute("class", "starter");
+  blocksArray[initialPositions[level].endBlue].setAttribute("id", "endBlue");
+  blocksArray[initialPositions[level].endBlue].setAttribute("class", "starter");
 
-  blocksArray[14].setAttribute("id", "initGreen");
-  blocksArray[14].setAttribute("class", "starter");
-  blocksArray[35].setAttribute("id", "endGreen");
-  blocksArray[35].setAttribute("class", "starter");
+  blocksArray[initialPositions[level].initGreen].setAttribute("id", "initGreen");
+  blocksArray[initialPositions[level].initGreen].setAttribute("class", "starter");
+  blocksArray[initialPositions[level].endGreen].setAttribute("id", "endGreen");
+  blocksArray[initialPositions[level].endGreen].setAttribute("class", "starter");
 
-  blocksArray[30].setAttribute("id", "initPurple");
-  blocksArray[30].setAttribute("class", "starter");
-  blocksArray[28].setAttribute("id", "endPurple");
-  blocksArray[28].setAttribute("class", "starter");
+  blocksArray[initialPositions[level].initPurple].setAttribute("id", "initPurple");
+  blocksArray[initialPositions[level].initPurple].setAttribute("class", "starter");
+  blocksArray[initialPositions[level].endPurple].setAttribute("id", "endPurple");
+  blocksArray[initialPositions[level].endPurple].setAttribute("class", "starter");
 
-  blocksArray[10].setAttribute("id", "initYellow");
-  blocksArray[10].setAttribute("class", "starter");
-  blocksArray[26].setAttribute("id", "endYellow");
-  blocksArray[26].setAttribute("class", "starter");
+  blocksArray[initialPositions[level].initYellow].setAttribute("id", "initYellow");
+  blocksArray[initialPositions[level].initYellow].setAttribute("class", "starter");
+  blocksArray[initialPositions[level].endYellow].setAttribute("id", "endYellow");
+  blocksArray[initialPositions[level].endYellow].setAttribute("class", "starter");
 
-  blocksArray[20].setAttribute("id", "initAqua");
-  blocksArray[20].setAttribute("class", "starter");
-  blocksArray[25].setAttribute("id", "endAqua");
-  blocksArray[25].setAttribute("class", "starter");
+  blocksArray[initialPositions[level].initAqua].setAttribute("id", "initAqua");
+  blocksArray[initialPositions[level].initAqua].setAttribute("class", "starter");
+  blocksArray[initialPositions[level].endAqua].setAttribute("id", "endAqua");
+  blocksArray[initialPositions[level].endAqua].setAttribute("class", "starter");
   addEventListenerMouseDown(initRed);
   addEventListenerMouseDown(initBlue);
   addEventListenerMouseDown(initGreen);
   addEventListenerMouseDown(initPurple);
   addEventListenerMouseDown(initYellow);
   addEventListenerMouseDown(initAqua);
+  addEventListenerMouseOver();
 }
 
 function addEventListenerMouseDown(init) {
+  console.log("mousedown activated");
   init.addEventListener("mousedown", function() {
     color = this.id.replace("init", "").toLowerCase();
     this.className = "block"+" "+ color;
   });
-  addEventListenerMouseOver();
 }
 
 function addEventListenerMouseOver() {
+  console.log("mouse over activated");
   blocksArray.forEach(function(block, i) {
     block.addEventListener("mouseover", function() {
       if(event.buttons === 1 &&
@@ -180,35 +333,50 @@ function checkWin() {
       display.setAttribute("class","display"+" win");
       buttonNext.setAttribute("class", "buttonNext"+" win");
       buttonReplay.setAttribute("class", "buttonReplay"+" win");
-      addButtonNextToListen();
-      addButtonReplayToListen();
     }
   }
 }
 
 function addButtonNextToListen() {
+  console.log("added button");
   buttonNext.addEventListener("click", function() {
     console.log("change to second array");
+    replay();
+    for (var i=0; i<36; i++) {
+      blocksArray[i].setAttribute("class","block");
+    }
+    blocksArray[initialPositions[level].initRed].removeAttribute("id","initRed");
+    blocksArray[initialPositions[level].endRed].removeAttribute("id","endRed");
+    blocksArray[initialPositions[level].initBlue].removeAttribute("id","initBlue");
+    blocksArray[initialPositions[level].endBlue].removeAttribute("id","endBlue");
+    blocksArray[initialPositions[level].initGreen].removeAttribute("id","initGreen");
+    blocksArray[initialPositions[level].endGreen].removeAttribute("id","endGreen");
+    blocksArray[initialPositions[level].initPurple].removeAttribute("id","initPurple");
+    blocksArray[initialPositions[level].endPurple].removeAttribute("id","endPurple");
+    blocksArray[initialPositions[level].initYellow].removeAttribute("id","initYellow");
+    blocksArray[initialPositions[level].endYellow].removeAttribute("id","endYellow");
+    blocksArray[initialPositions[level].initAqua].removeAttribute("id","initAqua");
+    blocksArray[initialPositions[level].endAqua].removeAttribute("id","endAqua");
     level++;
+    console.log(level);
+    blocksStarter();
   })
 }
 
 function addButtonReplayToListen() {
   buttonReplay.addEventListener("click", function() {
-    reset(allColors);
-    display.setAttribute("class","display");
-    buttonNext.setAttribute("class", "buttonNext");
-    buttonReplay.setAttribute("class", "buttonReplay");
-    console.log("pass reset colors");
-    // setTimeout();
-    console.log("pass setTimeOut");
-    time=20;
-    startTiming();
-    console.log("pass startTiming");
-    // addTimerDisplay();
-    // console.log("pass addTimerDispaly");
-    /*HOW to reset the timing???????????????????????????????????????????????????*/
+    replay();
   })
+}
+
+function replay() {
+  reset(allColors);
+  display.setAttribute("class","display");
+  buttonNext.setAttribute("class", "buttonNext");
+  buttonReplay.setAttribute("class", "buttonReplay");
+  // time=20;
+  // startTiming();
+  /*HOW to reset the timing???????????????????????????????????????????????????*/
 }
 
 function addResetButton() {
@@ -254,37 +422,37 @@ function addEventListenerMouseUp() {
   blocksArray.forEach(function(block, i) {
     block.addEventListener("mouseup", function() {
       switch(true) {
-        case ((this.id!=="endRed")&& (blocksArray[0].className==="block red")):
+        case ((this.id!=="endRed")&& (blocksArray[initialPositions[level].initRed].className==="block red")):
         reset(["red"]);
-        blocksArray[0].className==="starter";
+        blocksArray[initialPositions[level].initRed].className==="starter";
         break;
-        case ((this.id!=="endBlue")&& (blocksArray[8].className==="block blue")):
+        case ((this.id!=="endBlue")&& (blocksArray[initialPositions[level].initBlue].className==="block blue")):
         reset(["blue"]);
-        blocksArray[8].className==="starter";
+        blocksArray[initialPositions[level].initBlue].className==="starter";
         break;
-        case ((this.id!=="endGreen")&&(blocksArray[14].className==="block green")):
+        case ((this.id!=="endGreen")&&(blocksArray[initialPositions[level].initGreen].className==="block green")):
         reset(["green"]);
-        blocksArray[14].className==="starter";
+        blocksArray[initialPositions[level].initGreen].className==="starter";
         break;
-        case ((this.id!=="endPurple")&&(blocksArray[30].className==="block purple")):
+        case ((this.id!=="endPurple")&&(blocksArray[initialPositions[level].initPurple].className==="block purple")):
         reset(["purple"]);
-        blocksArray[30].className==="starter";
+        blocksArray[initialPositions[level].initPurple].className==="starter";
         break;
-        case ((this.id!=="endYellow")&&(blocksArray[10].className==="block yellow")):
+        case ((this.id!=="endYellow")&&(blocksArray[initialPositions[level].initYellow].className==="block yellow")):
         reset(["yellow"]);
-        blocksArray[10].className==="starter";
+        blocksArray[initialPositions[level].initYellow].className==="starter";
         break;
-        case ((this.id!=="endAqua")&&(blocksArray[20].className==="block aqua")):
+        case ((this.id!=="endAqua")&&(blocksArray[initialPositions[level].initAqua].className==="block aqua")):
         reset(["aqua"]);
-        blocksArray[20].className==="starter";
+        blocksArray[initialPositions[level].initAqua].className==="starter";
         break;
         default:
-        blocksArray[0].className="starter";
-        blocksArray[8].className="starter";
-        blocksArray[14].className="starter";
-        blocksArray[30].className="starter";
-        blocksArray[10].className="starter";
-        blocksArray[20].className="starter";
+        blocksArray[initialPositions[level].initRed].className="starter";
+        blocksArray[initialPositions[level].initBlue].className="starter";
+        blocksArray[initialPositions[level].initGreen].className="starter";
+        blocksArray[initialPositions[level].initPurple].className="starter";
+        blocksArray[initialPositions[level].initYellow].className="starter";
+        blocksArray[initialPositions[level].initAqua].className="starter";
       }
     });
   });
@@ -306,17 +474,6 @@ function reset(colors) {
 
 // }
 
-// $(function() {
-//   console.log("jQuery Loaded");
-//     var x = 50;
-//     var xId=setInterval(function(){
-//       $('html').css('background-position', x + 'px 0');
-//       x-=1;
-//       if (x===0) {
-//         clearInterval(xId);
-//       }
-//     }, 10);
-// });
 
 
 
